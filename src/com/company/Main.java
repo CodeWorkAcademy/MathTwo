145a70d9587743733c6fd75cff1707e8e881fc5e
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-23 16:05:56 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    double mynumber, myothernumber;
+    mynumber =2;
+    myothernumber=1.7938;
+	System.out.println("My number =" +mynumber +" My other numbe ="+ myothernumber);
+    }
+}
